5744520e9440a933d1495074e6db3e0a02205bb9
jmattaa <mattajonathan1@gmail.com>
2026-01-05 23:29:01 +0100

    update quickbuild to qvickbuild

--- scripts/qvickbuild_gen_complcmds.py
+++ scripts/qvickbuild_gen_complcmds.py
@@ -5,12 +5,12 @@ import subprocess
 import json
 
 '''
-hopefully quickbuild will be able to just give output of commands in the future
+hopefully qvickbuild will be able to just give output of commands in the future
 and render this function redunant
 '''
 
 
-def run_quickbuild_cmd(cmd_name):
+def run_qvickbuild_cmd(cmd_name):
     result = subprocess.run(
         ["quickbuild", "--log-quiet", cmd_name],
         stdout=subprocess.PIPE,
@@ -22,13 +22,13 @@ def run_quickbuild_cmd(cmd_name):
 
 project_dir = os.path.abspath(".")
 
-src_files = run_quickbuild_cmd("echocfiles")
+src_files = run_qvickbuild_cmd("echocfiles")
 if src_files:
     src_files = src_files[0].split()
 else:
     src_files = []
 
-compile_flags_lines = run_quickbuild_cmd("echocompileflags")
+compile_flags_lines = run_qvickbuild_cmd("echocompileflags")
 compile_flags = " ".join(compile_flags_lines) if compile_flags_lines else ""
 
 entries = []
